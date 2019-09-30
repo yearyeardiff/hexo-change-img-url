1341487bb243f3a0ce209db1c1363c75f5791dd4
zhangch <zhangchenghao@imdada.cn>
2019-09-30 15:32:15 +0800

    支持像images/test.png这样的相对路径

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const imageRegex = /(<img\s[^>]*?src(\s+)?=[\s"'])\.(.+)/gi
+const imageRegex = /(<img\s[^>]*?src(\s+)?=[\s"'])[\.]?[/]?(.+)/gi
 hexo.extend.filter.register('after_post_render', function(data) {
 	if(data.layout=='post'||data.layout=='page'||data.layout=='about'){
-		data.content=data.content.replace(imageRegex, '$1' + '$3"');
+		data.content=data.content.replace(imageRegex, '$1' + '/' + '$3"');
 	}
     return data;
 });
